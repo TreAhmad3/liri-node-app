7b3c950512f1b624ad97272c4d990fa337664c07
Tre' Anderson-Davis <macuser@MacUsers-MBP-2.domain_not_set.invalid>
2017-02-16 00:44:09 -0500

    working liri app

--- liri.js
+++ liri.js
@@ -6,6 +6,13 @@ var command = input[2];
 
 var itemName = input[3];
 
+// OMDB -------
+
+var movieKey = "1d902091f624dd0ac10d4954cefe5a14"
+
+var mdb = require('moviedb')(movieKey);
+
+var movieQuery = "";
 
 
 // console.log(itemName);
@@ -18,10 +25,12 @@ var keyFile = require("./keys.js");
 
 var keyList = keyFile.twitterKeys;
 
+var T = new Twitter(keyList);
+
 // console.log(keyFile);
 
 var params = {
-	count: 20,
+    count: 20,
 }
 
 
@@ -32,62 +41,111 @@ var spotify = require('spotify');
 
 var querySearch = "";
 
-var T = new Twitter(keyList);
-
-if (command == "my-tweets"){
-	
-		T.get('statuses/user_timeline', params , function(error, tweets, response){
-			if(error) throw error;
-			var tweetArr = [];
-			for (var i = 0; i < tweets.length; i++){
-				var userTweets = tweets[i].text; //how do you get the time the tweet was posted also???
-				var tweetTime = tweets[i].created_at;
-				console.log(userTweets + " tweeted at " + tweetTime);
-				console.log("--------------------")
-				tweetArr.push(userTweets); //might use to store in the txt file if needed.
-			}
-			// console.log(tweetArr[0]);	
-		});
-} else if (command == "spotify-this-song"){
-	if (querySearch != ""){
-		for (var i = 3; i < input.length; i++){
-			querySearch = querySearch + " " + input[i];
-		}
-
-		console.log("Searching for" + querySearch);
-		console.log("--------------------");
-
-		querySearch = querySearch.trim();
-
-		spotify.search({type: 'track', query: querySearch}, function(err, data){
-			var trackItemsArr = data["tracks"]["items"];
-			
-			if (!err) {
-				for (var i = 0; i < trackItemsArr.length; i++){
-					var trackName = data["tracks"]["items"][i]["name"];
-					var albumName = data["tracks"]["items"][i]["album"]["name"];
-					var artistName = data["tracks"]["items"][i]["album"]["artists"][0]["name"];
-					var previewLink = data["tracks"]["items"][i]["preview_url"];
 
 
-					if (trackName.toUpperCase() == querySearch.toUpperCase()){
-						console.log("Artist: " + artistName);
-						console.log("Track Name: " + trackName);
-						console.log("Preview Link: " + previewLink);
-						console.log("Album: " + albumName);
-						console.log("--------------------");
-						
-						break;
-					}
-				}
-			} else {
-				console.log('Error occured: ' + err);
-				return;
-			}
-		});
-	} else {
-		console.log("Please provide a song name!");
-	}
+if (command == "my-tweets") {
 
+    T.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (error) throw error;
+        var tweetArr = [];
+        for (var i = 0; i < tweets.length; i++) {
+            var userTweets = tweets[i].text; //how do you get the time the tweet was posted also???
+            var tweetTime = tweets[i].created_at;
+            console.log(userTweets + " tweeted at " + tweetTime);
+            console.log("--------------------")
+            tweetArr.push(userTweets); //might use to store in the txt file if needed.
+        }
+        // console.log(tweetArr[0]);	
+    });
+} else if (command == "spotify-this-song") {
+    if (querySearch != " ") {
+        for (var i = 3; i < input.length; i++) {
+            querySearch = querySearch + " " + input[i];
+        }
+
+        console.log("Searching for" + querySearch);
+        console.log("......................");
+
+        querySearch = querySearch.trim();
+
+        spotify.search({ type: 'track', query: querySearch }, function(err, data) {
+            var trackItemsArr = data["tracks"]["items"];
+
+            if (!err) {
+                for (var i = 0; i < trackItemsArr.length; i++) {
+                    var trackName = data["tracks"]["items"][i]["name"];
+                    var albumName = data["tracks"]["items"][i]["album"]["name"];
+                    var artistName = data["tracks"]["items"][i]["album"]["artists"][0]["name"];
+                    var previewLink = data["tracks"]["items"][i]["preview_url"];
+
+
+                    if (trackName.toUpperCase() == querySearch.toUpperCase()) {
+                        console.log("====================");
+                        console.log("Artist: " + artistName);
+                        console.log("--------------------");
+                        console.log("Track Name: " + trackName);
+                        console.log("--------------------");
+                        console.log("Preview Link: " + previewLink);
+                        console.log("--------------------");
+                        console.log("Album: " + albumName);
+                        console.log("--------------------");
+
+                        break;
+                    }
+                }
+            } else {
+                console.log('Error occured: ' + err);
+                return;
+            }
+        });
+    } else {
+        console.log("Please provide a song name!");
+    }
+    // ---------- MOVIE DB ------------
+} else if (command == "movie-this") {
+    if (querySearch != " ") {
+
+        for (var i = 3; i < input.length; i++) {
+            movieQuery = movieQuery + " " + input[i];
+        }
+
+        console.log("Searching for" + movieQuery);
+        console.log("......................");
+
+        movieQuery = movieQuery.trim().split(' ').join('+');
+
+
+        mdb.searchMovie({ query: movieQuery }, function(err, res) {
+            var movieItemsArr = res["results"];
+            if (!err) {
+                for (var i = 0; i < movieItemsArr.length; i++) {
+                    var movieTitle = res["results"][i]["original_title"];
+                    var movieYear = res["results"][i]["release_date"];
+                    var movieRating = res["results"][i]["vote_average"];
+                    var moviePlot = res["results"][i]["overview"];
+
+                    movieTitle = movieTitle.trim().split(' ').join('+');
+
+                    if (movieTitle.toUpperCase() == movieQuery.toUpperCase()) {
+                        console.log("====================");
+                        console.log("Movie Title: " + movieTitle.split('+').join(' '));
+                        console.log("--------------------");
+                        console.log("This movie was released: " + movieYear);
+                        console.log("--------------------");
+                        console.log("This movie earned (on average) " + movieRating + " out of 10.");
+                        console.log("--------------------");
+                        console.log("Plot: " + moviePlot);
+                        console.log("--------------------");
+
+                        break;
+                    }
+                }
+            } else {
+                console.log('Error occured: ' + err);
+                return;
+            }
+        });
+    } else {
+        console.log("Please provide a song name!");
+    }
 }
-
